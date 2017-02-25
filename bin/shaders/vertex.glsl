1089910e5436f1874bf93edfe3742115eec0215d
AshishBhattarai <codemyimagination@gmail.com>
2017-02-26 00:08:59 +0545

    Updated shaders

--- bin/shaders/vertex.glsl
+++ bin/shaders/vertex.glsl
@@ -11,13 +11,15 @@ uniform mat4 projectionMatrix;
 uniform mat4 compositeMatrix;
 uniform mat4 viewMatrix;
 uniform bool facing;
+uniform vec2 frame;
+uniform vec2 spriteSize;
 
 void main(void) {
 
 	gl_Position = projectionMatrix * viewMatrix * compositeMatrix * vec4(vertexPos,1.0);
 
 	if(facing)
-		pass_textCoords = textCoords;
+		pass_textCoords = frame + (textCoords * spriteSize);
 	else
-		pass_textCoords = textCoords2;
+		pass_textCoords = frame + (textCoords2 * spriteSize);
 }
